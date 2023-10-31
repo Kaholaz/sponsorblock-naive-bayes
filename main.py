0bb12812a1b662769078f57b9eef6748152a9e26
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-10-31 20:20:55 +0100

    feat: add a crude bayes classifier

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bayes import NaiveBayesClassifier
+
 training_data = [
     ("spam email content 1", "spam"),
     ("ham email content 1", "ham"),
@@ -11,7 +12,12 @@ testing_data = [
     "ham spam spam",
 ]
 
+
 def main():
     bayes = NaiveBayesClassifier(training_data=training_data)
     bayes.train()
     bayes.test(testing_data=testing_data)
+
+
+if __name__ == "__main__":
+    main()
