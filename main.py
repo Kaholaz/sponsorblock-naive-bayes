d41b5bee76b80271cab7813358ed520c3adff3ec
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-10-31 11:29:56 +0100

    chore: create a requirements file

---
+++ main.py
@@ -0,0 +1,17 @@
+from bayes import NaiveBayesClassifier
+training_data = [
+    ("spam email content 1", "spam"),
+    ("ham email content 1", "ham"),
+    ("spam email content 2", "spam"),
+    ("ham email content 2", "ham"),
+]
+
+testing_data = [
+    "spam email content 3",
+    "ham spam spam",
+]
+
+def main():
+    bayes = NaiveBayesClassifier(training_data=training_data)
+    bayes.train()
+    bayes.test(testing_data=testing_data)
