1663ce639bca035911c3de55b36dbea2bcd3aa4d
Navid Muradi <19255699+Navish97@users.noreply.github.com>
2023-11-01 18:39:41 +0100

    implemented usage of the ad transcription method into main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from transcribe import *
 
 def main():
     # transcribe_data()
+    #transcribe_ads_data()
     training_data = []
     for index in range(1, 3):
         with open(f"transcriptions/{index}.csv", "r") as f:
@@ -14,10 +15,12 @@ def main():
     with open(f"transcriptions/0.csv", "r") as f:
         data = pd.read_csv(f)
     testing_data = list(zip(data["word"], data["start"], data["ad"]))
-    bayes = NaiveBayesClassifier(training_data=testing_data)
+
+    bayes = NaiveBayesClassifier(training_data=training_data)
 
     bayes.train()
     bayes.test(testing_data=testing_data)
+
 
 
 def transcribe_data():
@@ -33,6 +36,19 @@ def transcribe_data():
         transcription = transcribe(video_id)
         with open(f"transcriptions/{index}.csv", "w") as f:
             transcription.to_csv(f, index=False)
+
+def transcribe_ads_data():
+    videos = [
+            "https://www.youtube.com/watch?v=mXBzBFxe00o",
+        ]
+    for index, video in enumerate(videos):
+        video_id = get_video_id(video)
+        transcription = transcribe_ads(video_id)
+        if not transcription.empty:
+            with open(f"transcriptions/{video_id}_ads.csv", "w") as f:
+                transcription.to_csv(f, index=False)
+        else:
+            print(f"No ads to transcribe for video {video_id}")
 
 
 if __name__ == "__main__":
