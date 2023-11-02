887f02cb54d0e24a23f9a9a5a77d2f9be0764db0
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-11-02 13:45:09 +0100

    feat: add seperate filehandler class

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from bayes import NaiveBayesClassifier
+from files import TranscriptionFileHandler, FileType
 from transcribe import *
 
 
 def main():
-    # transcribe_data()
     #transcribe_ads_data()
-    training_data = []
-    for index in range(1, 3):
-        with open(f"transcriptions/{index}.csv", "r") as f:
-            data = pd.read_csv(f)
-        training_data.extend(list(zip(data["word"], data["start"], data["ad"])))
+    training_videos = [
+        "https://www.youtube.com/watch?v=mXBzBFxe00o",
+        "https://www.youtube.com/watch?v=jCuEBVbmPcA",
+        "https://www.youtube.com/watch?v=r4NL2e6jJ04",
+    ]
 
-    testing_data = []
-    with open(f"transcriptions/0.csv", "r") as f:
-        data = pd.read_csv(f)
-    testing_data = list(zip(data["word"], data["start"], data["ad"]))
+    testing_videos = [
+        "https://www.youtube.com/watch?v=8rofPMY54Ko&t=207s",
+    ]
+
+    files = TranscriptionFileHandler()
+
+    if not files.check_resouce_exists():
+        files.transcribe_and_save_videos(training_videos, FileType.TRAINING)
+        files.transcribe_and_save_videos(testing_videos, FileType.TESTING)
+
+    training_data = files.load_data(FileType.TRAINING)
+    testing_data = files.load_data(FileType.TESTING)
 
     bayes = NaiveBayesClassifier(training_data=training_data)
 
@@ -22,20 +30,6 @@ def main():
     bayes.test(testing_data=testing_data)
 
 
-
-def transcribe_data():
-    videos = [
-        "https://www.youtube.com/watch?v=8rofPMY54Ko&t=207s",
-        "https://www.youtube.com/watch?v=mXBzBFxe00o",
-        "https://www.youtube.com/watch?v=jCuEBVbmPcA",
-        "https://www.youtube.com/watch?v=r4NL2e6jJ04",
-    ]
-
-    for index, video in enumerate(videos):
-        video_id = get_video_id(video)
-        transcription = transcribe(video_id)
-        with open(f"transcriptions/{index}.csv", "w") as f:
-            transcription.to_csv(f, index=False)
 
 def transcribe_ads_data():
     videos = [
