7ac716059362de55d2b63dea9a582c21ccff54b1
Navid Muradi <19255699+Navish97@users.noreply.github.com>
2023-11-04 00:31:08 +0100

    added description to trim_sort_sponsorTimes.py

--- trim_sort_sponsorTimes.py
+++ trim_sort_sponsorTimes.py
@@ -1,3 +1,12 @@
+"""
+    sponsorTimes.csv was fetched from https://sponsor.ajay.app/database ahead of time.
+    The sponsorTimes.csv file contains sponsor segment timestamps for a lot of youtube videos.
+    This code can be used to trim off unnecessary columns from sponsorTimes.csv,
+        then sort videoID by views in descending order.
+    The sponsorTimes.csv can then be used in youtube_transcription_fetcher.py to fetch the manual english
+        transcript for each yt video id, and also assign each sentence in each transcript as an ad or not.
+"""
+
 import pandas as pd
 chunk_size = 50000
 chunks = []
