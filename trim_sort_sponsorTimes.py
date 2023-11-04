261255624098cb5a80e3676a903d709a78205d11
Navid Muradi <19255699+Navish97@users.noreply.github.com>
2023-11-04 20:57:04 +0100

    added further processing to trim_sort_sponsorTimes.py

--- trim_sort_sponsorTimes.py
+++ trim_sort_sponsorTimes.py
@@ -3,6 +3,8 @@
     The sponsorTimes.csv file contains sponsor segment timestamps for a lot of youtube videos.
     This code can be used to trim off unnecessary columns from sponsorTimes.csv,
         then sort videoID by views in descending order.
+    It also preprocesses the data to remove rows depending on category, and number of votes.
+    This ensures that only sponsor categories, and segments are verified by users.
     The sponsorTimes.csv can then be used in youtube_transcription_fetcher.py to fetch the manual english
         transcript for each yt video id, and also assign each sentence in each transcript as an ad or not.
 """
@@ -16,8 +18,9 @@ directory = ".idea/"
 sponsorTimes_path = directory + "sponsorTimes.csv"
 processed_sponsorTimes_path = directory + "processed_sponsorTimes.csv"
 
-for chunk in pd.read_csv(sponsorTimes_path, usecols=["videoID", "startTime", "endTime", "views"], chunksize=chunk_size):
-    chunks.append(chunk)
+for chunk in pd.read_csv(sponsorTimes_path, usecols=["videoID", "startTime", "endTime", "views", "votes", "category"], chunksize=chunk_size):
+    filtered_chunk = chunk[(chunk["votes"] >= 5) & (chunk["category"] == "sponsor")]
+    chunks.append(filtered_chunk)
 
 df_combined = pd.concat(chunks)
 
