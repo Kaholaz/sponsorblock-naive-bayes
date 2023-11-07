94d636f2dd7e7e77bfa54a55cd42df4d888658f7
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-11-07 01:36:50 +0100

    refac: move all plot methods to bottom

--- bayes.py
+++ bayes.py
@@ -121,48 +121,6 @@ class NaiveBayesClassifier:
             {k: v * (1 / self.prior_ham) for k, v in self.ham_word_counts.items()},
         )
 
-    def visualize_words(self) -> None:
-        # Generate word cloud for spam words
-        spam_wordcloud = WordCloud(
-            width=800, height=400, background_color="white"
-        ).generate_from_frequencies(self.spam_word_counts)
-
-        # Generate word cloud for ham words
-        ham_wordcloud = WordCloud(
-            width=800, height=400, background_color="white"
-        ).generate_from_frequencies(self.ham_word_counts)
-
-        # Create subplots
-        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-
-        # Display the spam word cloud
-        ax1.imshow(spam_wordcloud, interpolation="bilinear")
-        ax1.set_title("Spam Word Cloud")
-        ax1.axis("off")
-
-        # Display the ham word cloud
-        ax2.imshow(ham_wordcloud, interpolation="bilinear")
-        ax2.set_title("Ham Word Cloud")
-        ax2.axis("off")
-
-        # Show the plot
-        plt.tight_layout()
-        plt.show()
-
-    def visualize_data_summary(self) -> None:
-        # Create a figure for the pie chart
-        plt.figure(figsize=(6, 6))
-
-        # Create a pie chart for Spam vs. Ham Distribution
-        plt.pie(
-            [self.total_spam, self.total_ham],
-            labels=["Spam", "Ham"],
-            autopct="%1.1f%%",
-            startangle=90,
-        )
-        plt.title("Spam vs. Ham Distribution")
-
-        plt.show()
 
     def _classify(self, words: list[Word]) -> float:
         """
@@ -193,18 +151,6 @@ class NaiveBayesClassifier:
 
         return spam_probability
     
-    def plot_spam_score(self, timestamps, spam_score):
-        plt.figure(figsize=(10, 6))
-        plt.plot(timestamps, spam_score, label="Spam")
-
-        plt.title("Spam Score")
-        plt.xlabel("Time")
-        plt.ylabel("Score")
-
-        plt.legend()
-
-        plt.show()
-
     def test(
         self, testing_data: list, window_size=WINDOW_SIZE, ham_threshold=HAM_THRESHOLD
     ) -> None:
@@ -249,4 +195,59 @@ class NaiveBayesClassifier:
         
         self.plot_spam_score(timestamps, spam_score)
 
+
+    def visualize_words(self) -> None:
+        # Generate word cloud for spam words
+        spam_wordcloud = WordCloud(
+            width=800, height=400, background_color="white"
+        ).generate_from_frequencies(self.spam_word_counts)
+
+        # Generate word cloud for ham words
+        ham_wordcloud = WordCloud(
+            width=800, height=400, background_color="white"
+        ).generate_from_frequencies(self.ham_word_counts)
+
+        # Create subplots
+        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+        # Display the spam word cloud
+        ax1.imshow(spam_wordcloud, interpolation="bilinear")
+        ax1.set_title("Spam Word Cloud")
+        ax1.axis("off")
+
+        # Display the ham word cloud
+        ax2.imshow(ham_wordcloud, interpolation="bilinear")
+        ax2.set_title("Ham Word Cloud")
+        ax2.axis("off")
+
+        # Show the plot
+        plt.tight_layout()
+        plt.show()
+
+    def visualize_data_summary(self) -> None:
+        # Create a figure for the pie chart
+        plt.figure(figsize=(6, 6))
+
+        # Create a pie chart for Spam vs. Ham Distribution
+        plt.pie(
+            [self.total_spam, self.total_ham],
+            labels=["Spam", "Ham"],
+            autopct="%1.1f%%",
+            startangle=90,
+        )
+        plt.title("Spam vs. Ham Distribution")
+
+        plt.show()
+
+    def plot_spam_score(self, timestamps, spam_score):
+        plt.figure(figsize=(10, 6))
+        plt.plot(timestamps, spam_score, label="Spam")
+
+        plt.title("Spam Score")
+        plt.xlabel("Time")
+        plt.ylabel("Score")
+
+        plt.legend()
+
+        plt.show()
 
