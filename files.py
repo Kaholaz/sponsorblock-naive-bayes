431a78489c705fb85789a933979ba44560a38353
Arunan Gnanasekaran <arunan2212@gmail.com>
2023-11-09 16:14:34 +0100

    make classify and train use preprocess

--- files.py
+++ files.py
@@ -9,6 +9,7 @@ from transcribers.youtube_transcription_fetcher import fetch_transcript
 class FileType(Enum):
     TRAINING = "training"
     TESTING = "testing"
+    PREPROCESSED = "preprocessed"
     ROOT = ""
 
 
@@ -19,6 +20,12 @@ class TranscriptionFileHandler:
     def __init__(self, path=DEFAULT_TRANSCRIPTION_PATH) -> None:
         self.path = path
 
+    def dump_preprocessed_words(self, text: DataFrame) -> None:
+        if not os.path.exists(self.path + FileType.PREPROCESSED.value):
+            os.makedirs(self.path + FileType.PREPROCESSED.value)
+        with open(f"{self.path}{FileType.PREPROCESSED.value}/dump.csv", "w", encoding="UTF-8") as f:
+            text.to_csv(f)
+
     def load_data(self, file_type: FileType) -> DataFrame:
         files = os.listdir(f"{self.path}{file_type.value}")
 
@@ -27,7 +34,7 @@ class TranscriptionFileHandler:
             with open(f"{self.path}{file_type.value}/{file}", encoding="UTF-8") as f:
                 data = pd.read_csv(f)
             frames.append(data)
-        
+
         return frames
 
     def transcribe_and_save_videos(self, videos: [str], file_type: FileType) -> None:
@@ -54,5 +61,18 @@ class TranscriptionFileHandler:
 
         return transcription
 
-    def check_resouce_exists(self) -> bool:
-        return os.path.exists(self.path)
+    def check_resouce_exists(self, file_type: FileType = FileType.ROOT) -> bool:
+        return os.path.exists(self.path + file_type.value)
+
+def transcribe_ads_data():
+    videos = [
+        "https://www.youtube.com/watch?v=mXBzBFxe00o",
+    ]
+    for index, video in enumerate(videos):
+        video_id = get_video_id(video)
+        transcription = transcribe_ads(video_id)
+        if not transcription.empty:
+            with open(f"transcriptions/{video_id}_ads.csv", "w") as f:
+                transcription.to_csv(f, index=False)
+        else:
+            print(f"No ads to transcribe for video {video_id}")
