5fbfa79fc9d14231aebf88ca226e1b150916b89e
Navid Muradi <19255699+Navish97@users.noreply.github.com>
2023-11-09 16:10:44 +0100

    method for getting single transcription as dataframe, tries youtube api first otherwise uses WhisperX

--- files.py
+++ files.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 from enum import Enum
 import os
 from transcribers.transcribe import transcribe_video, get_video_id
+from transcribers.youtube_transcription_fetcher import fetch_transcript
 
 
 class FileType(Enum):
@@ -10,7 +11,9 @@ class FileType(Enum):
     TESTING = "testing"
     ROOT = ""
 
+
 DEFAULT_TRANSCRIPTION_PATH = "transcriptions/"
+
 
 class TranscriptionFileHandler:
     def __init__(self, path=DEFAULT_TRANSCRIPTION_PATH) -> None:
@@ -24,7 +27,7 @@ class TranscriptionFileHandler:
             with open(f"{self.path}{file_type.value}/{file}", encoding="UTF-8") as f:
                 data = pd.read_csv(f)
             frames.append(data)
-        
+
         return pd.concat(frames)
 
     def transcribe_and_save_videos(self, videos: [str], file_type: FileType) -> None:
@@ -35,9 +38,21 @@ class TranscriptionFileHandler:
             video_id = get_video_id(video)
             transcription = transcribe_video(video_id)
             with open(
-                f"{self.path}{file_type.value}/{index}.csv", "w", encoding="UTF-8"
+                    f"{self.path}{file_type.value}/{index}.csv", "w", encoding="UTF-8"
             ) as f:
                 transcription.to_csv(f)
+
+    @staticmethod
+    def get_transcription(video: str) -> DataFrame:
+        video_id = get_video_id(video)
+        try:
+            transcription = fetch_transcript(video_id)
+        except Exception as e:
+            print("An error occurred while fetching the transcript: ", e)
+            print("\nDownloading and transcribing the video with WhisperX.\n")
+            transcription = transcribe_video(video_id)
+
+        return transcription
 
     def check_resouce_exists(self) -> bool:
         return os.path.exists(self.path)
