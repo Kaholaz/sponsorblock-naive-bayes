f3c967f9aa8f29825a12fb5afd11f4c443f29dfb
Navid Muradi <19255699+Navish97@users.noreply.github.com>
2023-11-21 14:08:40 +0100

    transcriber.py added logic for xor args

--- transcriber.py
+++ transcriber.py
@@ -5,13 +5,20 @@ from config import ROOT_DIR
 SAVE_PATH = ROOT_DIR+"/transcriptions/transcription.csv"
 
 if __name__ == "__main__":
-    argparse = argparse.ArgumentParser(description="Transcribes a youtube video or audio file, and saves transcription to a path.")
+    parser = argparse.ArgumentParser(description="Transcribes a youtube video or audio file, and saves transcription "
+                                                 "to a path.")
 
-    argparse.add_argument("--video", type=str, help="For transcribing YouTube video. Can either be a YouTube video ID or a URL.", required=False)
-    argparse.add_argument("--audio_path", type=str, help="For transcribing a local audio file. Has to be absolute path of the audio file.", required=False)
-    argparse.add_argument("--save_path", type=str, help="Optional full file path to save to, including filetype. If ommitted, defaults the save path to /transcriptions folder in project root.", required=False, default=SAVE_PATH)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--video", type=str, help="For transcribing YouTube video. Can either be a YouTube video ID or "
+                                                 "a URL.")
+    group.add_argument("--audio_path", type=str, help="For transcribing a local audio file. Has to be absolute path "
+                                                      "of the audio file.")
 
-    args = argparse.parse_args()
+    parser.add_argument("--save_path", type=str, help="Optional full file path to save to, including filetype. If "
+                                                      "ommitted, defaults the save path to /transcriptions folder in "
+                                                      "project root.", required=False, default=SAVE_PATH)
+
+    args = parser.parse_args()
 
     if args.video:
         df = transcriber.get_transcription(args.video)
